fc385c4c7f11824270f804c8e21cfd9c0d63f873
sekujaat <sekuktl08@gmail.com>
2026-01-14 15:21:05 +0530

    Update MainApplication.kt

--- mobile/android/app/src/main/java/com/synceclean/MainApplication.kt
+++ mobile/android/app/src/main/java/com/synceclean/MainApplication.kt
@@ -1,27 +1,39 @@
 package com.synceclean
 
 import android.app.Application
-import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactHost: ReactHost by lazy {
-    getDefaultReactHost(
-      context = applicationContext,
-      packageList =
-        PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
-        },
-    )
-  }
+    private val mReactNativeHost: ReactNativeHost =
+        object : DefaultReactNativeHost(this) {
 
-  override fun onCreate() {
-    super.onCreate()
-    loadReactNative(this)
-  }
+            override fun getPackages(): List<ReactPackage> {
+                // Auto-linked packages handle most cases; add manual packages here if needed.
+                return emptyList()
+            }
+
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+            override val isNewArchEnabled: Boolean
+                get() = false
+
+            override val isHermesEnabled: Boolean
+                get() = true
+        }
+
+    override fun getReactNativeHost(): ReactNativeHost = mReactNativeHost
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            DefaultNewArchitectureEntryPoint.load()
+        }
+    }
 }
