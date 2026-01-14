681cdd82874161923671a14c2cea01e61f7e927f
sekujaat <sekuktl08@gmail.com>
2026-01-14 22:02:01 +0530

    Update MainApplication.kt

--- mobile/android/app/src/main/java/com/synceclean/MainApplication.kt
+++ mobile/android/app/src/main/java/com/synceclean/MainApplication.kt
@@ -1,36 +1,49 @@
-package com.synceclean
+package com.syncecleaner
 
 import android.app.Application
+import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 import com.syncecleaner.BuildConfig
 
 class MainApplication : Application(), ReactApplication {
 
-    override val reactNativeHost: ReactNativeHost =
+    private val mReactNativeHost: ReactNativeHost =
         object : DefaultReactNativeHost(this) {
 
-            override fun getPackages(): List<ReactPackage> {
-                // Auto-linked packages; manual packages yahan add kar sakte ho agar future me zarurat ho.
-                return emptyList()
+            override fun getPackages(): MutableList<ReactPackage> {
+                return PackageList(this).packages
             }
 
-            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+            override fun getJSMainModuleName(): String = "index"
 
-            // New architecture / Hermes flags hata diye, kyunki BuildConfig me define nahi hain.
-            override val isNewArchEnabled: Boolean
-                get() = false
+            // new architecture flag yahin se control hoga
+            override fun isNewArchEnabled(): Boolean = IS_NEW_ARCHITECTURE_ENABLED
 
-            override val isHermesEnabled: Boolean
-                get() = true
+            override fun isHermesEnabled(): Boolean = BuildConfig.IS_HERMES_ENABLED
         }
+
+    override fun getReactNativeHost(): ReactNativeHost = mReactNativeHost
 
     override fun onCreate() {
         super.onCreate()
-        SoLoader.init(this, false)
-        // Yahan bhi New Architecture flag wala code hata diya.
+        SoLoader.init(this, /* native exopackage */ false)
+
+        if (IS_NEW_ARCHITECTURE_ENABLED) {
+            // If new architecture is enabled, load it
+            load()
+        }
+    }
+
+    companion object {
+        // yahi constant missing tha; ab har build me defined rahega
+        private const val IS_NEW_ARCHITECTURE_ENABLED: Boolean =
+            BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        // agar simple chahiye to yahan direct false bhi kar sakte:
+        // private const val IS_NEW_ARCHITECTURE_ENABLED: Boolean = false
     }
 }
